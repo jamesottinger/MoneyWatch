7e3213e843a1064df6a5af1694080565a21225e3
James Ottinger <ottinger.james@gmail.com>
2014-09-30 03:23:29 -0400

    code cleanup, licences
    - clean up JavaScript source - some code was proof-of-concept or used
    in my initial write/learning and can be discarded
    - clean up call list
    - remove references to YUI finance calls on client side
    - add license text
    - remove trailing stuff in css

--- js/moneywatch.js
+++ js/moneywatch.js
@@ -1,42 +1,28 @@
+//===============================================================================
+// Copyright (c) 2014, James Ottinger. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// MoneyWatch - https://github.com/jamesottinger/moneywatch
+//===============================================================================
+var moneyWatchX = '/x/moneywatch-relay.py';
+//var moneyWatchX = '/devmoney-x/moneywatch-relay.py';
+var activeInvRowId = '';
 
-//====================================================
-// INIT
-//====================================================
-var panelAddBank, panelIntBulk, panelInvSettings, panelUniversal, panelTransactions; // global variables (YUI panels)
+var panelUniversal, panelTransactions; // global variables (YUI panels)
 
 // YUI Components
 YUI({
      gallery: 'gallery-2011.06.01-20-18' // Last Gallery Build of this module
 }).use("panel", "dd-plugin", "autocomplete", "autocomplete-filters", "autocomplete-highlighters", "json", "json-parse", function(Y) {
 
-    panelAddBank = new Y.Panel({
-        srcNode: '#paneladdbank',
-        width: 725,
-        centered: true,
-        zIndex: 5,
-        headerContent: "Add New Bank Entry",
-        plugins: [Y.Plugin.Drag],
-        visible: false,
-        render: true
-    });
 
-    panelIntBulk = new Y.Panel({
-        srcNode: '#panelbulkinterest',
-        width: 420,
+    panelTransactions = new Y.Panel({
+        srcNode: '#paneltransactions',
+        width: 1200,
         centered: true,
         zIndex: 5,
-        headerContent: "Bulk Interest Entry",
-        plugins: [Y.Plugin.Drag],
-        visible: false,
-        render: true
-    });
-
-    panelInvSettings = new Y.Panel({
-        srcNode: '#panelinvestmentsettings',
-        width: 420,
-        centered: true,
-        zIndex: 5,
-        headerContent: "Investment Settings",
+        headerContent: "Transactions",
         plugins: [Y.Plugin.Drag],
         visible: false,
         render: true
@@ -53,170 +39,31 @@ YUI({
         render: true
     });
 
-    panelTransactions = new Y.Panel({
-        srcNode: '#paneltransactions',
-        width: 1200,
-        centered: true,
-        zIndex: 5,
-        headerContent: "Transactions",
-        plugins: [Y.Plugin.Drag],
-        visible: false,
-        render: true
-    });
-
-    Y.one('#autocategories').plug(Y.Plugin.AutoComplete, {
-        resultFilters: 'phraseMatch',
-        resultHighlighter: 'phraseMatch',
-        source: 'php-bank-categories-generate.php?&callback=parseAuto'
-    });
-
-    //  Accessing Yahoo Finance - http://yuilibrary.com/gallery/show/yql-finance
-    var symbols = ["FSEMX", "FSTMX", "FTBFX", "VFINX", "VBINX", "VHDYX", "VWEHX", "VISVX", "VIVAX"];
-   /*
-    function display(container, prices) {
-        var asset, content = "", c = Y.one(container);
-
-        for (asset in prices) {
-            if (prices.hasOwnProperty(asset)) {
-                content += asset + " -> ";
-                content += Y.JSON.stringify(prices[asset]);
-                content += "<br />";
-            }
-        }
-        c.setContent(content);
-    }
-
-    Y.YQL.Finance.getHistoricalQuotes(symbols, function (prices) {
-        display("#histQuotes", prices);
-    }, {
-        columns: ["Close", "Date"]
-    });
-
-    Y.YQL.Finance.getHistoricalQuotes(symbols, function (prices) {
-        display("#histQuotes2", prices);
-    }, {
-        columns: ["Close", "Date"],
-        frequency: "w",
-        startDate: "2010-10-01",
-        endDate: "2010-10-30"
-    });
-
-    Y.YQL.Finance.getQuotes(symbols, function (prices) {
-        display("#quotes", prices);
-    }, {
-        columns: ["PreviousClose", "LastTradePriceOnly", "Change", "LastTradeDate", "Name"]
-    });
-    */
-
 });
 
-// JQuery Components
-jQuery(function() {
-    jQuery("#datepicker1").datepicker({ dateFormat: 'yy-mm-dd' });
-    jQuery("#datepicker2").datepicker({ dateFormat: 'yy-mm-dd' });
-});
-
-
-
-
-//====================================================
-// AJAX
-//====================================================
-var moneyWatchX = '/x/moneywatch-relay.py';
-//var moneyWatchX = '/devmoney-x/moneywatch-relay.py';
-var xmlHttp = false;
-var requestType = "";
-var overobj;
-var activeInvRowId = '';
-
+// make the YUI control close my way
 var YUIcloseMarkup = '<span class="yui3-widget-button-wrapper"><a href="#" class="yui3-button yui3-button-close" onClick="utilClosePanel();"><span class="yui3-button-content"><span class="yui3-button-icon"></span></span></a></span>';
-
-
-function ajax_createXMLHttpRequest() {
-    if(window.XMLHttpRequest) {
-        xmlHttp = new XMLHttpRequest();
-    }
-    else if(window.ActiveXObject) {
-        try {
-            xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch(e) {
-            try {
-                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch(e) {
-                xmlHttp = false;
-            }
-        }
-    }
-}
-
-function ajax_handleStateChange()
-{
-    if(xmlHttp.readyState == 4) {
-        if(xmlHttp.status == 200 || xmlHttp.status == 304) {
-
-            if(requestType == "AJAX.BANK.GETTRANSACTIONS") {
-                jQuery('#rightcontent').html(xmlHttp.responseText);
-                // animate scroll to bottom
-                jQuery('#rightfixed').stop().animate({ scrollTop: jQuery('#rightbottom').offset().top },1600);
-            } else if(requestType == "AJAX.BANK.GETACCOUNTS") {
-                jQuery('#bankaccounts').html(xmlHttp.responseText);
-            } else if(requestType == "AJAX.INVESTMENT.GETACCOUNTS") {
-                jQuery('#investmentaccounts').html(xmlHttp.responseText);
-            } else if(requestType == "AJAX.BANK.MAKEBULKINTEREST") {
-                jQuery('#bulkinterestinner').html(xmlHttp.responseText);
-                panelIntBulk.show();
-            } else if(requestType == "AJAX.INVESTMENT.MAKEEDITSETTINGS") {
-                jQuery('#investmentsettingsinner').html(xmlHttp.responseText);
-                panelInvSettings.show();
-            } else if(requestType == "AJAX.BANK.MAKEBANKENTRY") {
-                jQuery('#bankentryinner').html(xmlHttp.responseText);
-                panelAddBank.show();
-            } else if(requestType == "AJAX.ALL.GETACCOUNTS") {
-                 jQuery('#rightcontent').html(xmlHttp.responseText);
-            }
-        }
-    }
-}
-
-// ===========================================================
-function runAction(in_action) {
-    if (in_action === 'BANK.MAKEBULKINTEREST') {
-        panelUniversal.set('width', 420);
-        panelUniversal.set('headerContent', "Bulk Interest Entry" + YUIcloseMarkup);
-        jQuery.post('/x/moneywatch-engine.py', {job: 'AJAX.INVESTMENT.GETSINGLETICKER', pu: utilPoisonURL()}, function(data) { jQuery('#paneluniversal-inner').html(data); panelUniversal.show(); });
-    } else if (in_action === 'U.IMPORTFILE.EDIT') {
-        panelUniversal.set('width', 420);
-        panelUniversal.set('headerContent', "Import Menu" + YUIcloseMarkup);
-        jQuery.post('/x/moneywatch-engine.py', {job: 'AJAX.IMPORTMENU', pu: utilPoisonURL()}, function(data) { jQuery('#paneluniversal-inner').html(data); panelUniversal.show(); });
-    }
-}
-
-function runActionInv(in_inv) {
-    jQuery.post('/x/moneywatch-engine.py', {job: 'AJAX.INV.GETTRANSACTIONS', ticker: in_inv, pu: utilPoisonURL()}, function(data) { jQuery('#rightcontent').html(data); });
-}
-
 
 function utilClosePanel() {
     panelUniversal.hide();
     jQuery('#paneluniversal-inner').html('');
 }
 
-function poisonURL() {
-    return new Date().getTime();
-}
-
-
 /*
--- I.SUMMARY.GET
--- I.ELECTION.GET
+===========================================================
+CALL LIST:
+
+I.SUMMARY.GET
+I.ELECTION.GET
 I.BULKADD.EDIT
 I.BULKADD.SAVE
-I.TICKERS.EDIT
-I.TICKERS.SAVE
+*- I.TICKERS.EDIT
+*- I.TICKERS.SAVE
 I.ENTRY.ADD
 I.ENTRY.EDIT
 I.ENTRY.SAVE
 I.ENTRY.DEL
+I.GRAPH.GET
 
 B.SUMMARY.GET
 B.ACCOUNT.GET
@@ -224,19 +71,18 @@ B.BULKADD.EDIT
 B.BULKADD.SAVE
 B.BULKINT.EDIT
 B.BULKINT.SAVE
-B.MYACCT.ON
-B.MYACCT.OFF
+*- B.MYACCT.ON
+*- B.MYACCT.OFF
 B.ENTRY.ADD
 B.ENTRY.EDIT
 B.ENTRY.SAVE
 B.ENTRY.DEL
 
--- U.IMPORTFILE.EDIT
--- U.IMPORTFILE.SAVE
--- U.UPDATEQUOTES
+*- U.IMPORTFILE.EDIT
+*- U.IMPORTFILE.SAVE
+U.UPDATEQUOTES
+===========================================================
 */
-
-// ===========================================================
 
 function init_loadaccounts() {
     sendCommand('I.SUMMARY.GET');
@@ -578,7 +424,6 @@ function sendBankDelete(in_bacctid, in_btransid) {
     }
 }
 
-
 function getBankAccount(in_bacctid) {
     jQuery.post(moneyWatchX,
         {job: 'B.ACCOUNT.GET', 'bacctid': in_bacctid, pu: poisonURL()},
@@ -600,45 +445,6 @@ function getBankAccount(in_bacctid) {
         }
     );
 }
-
-
-
-function formatCurrency(num) {
-    if (typeof num != 'undefined') {
-        num = num.toString().replace(/\$|\,/g, '');
-        if (isNaN(num)) num = "0";
-        sign = (num == (num = Math.abs(num)));
-        num = Math.floor(num * 100 + 0.50000000001);
-        cents = num % 100;
-        num = Math.floor(num / 100).toString();
-        if (cents < 10) cents = "0" + cents;
-        for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
-        num = num.substring(0, num.length - (4 * i + 3)) + ',' + num.substring(num.length - (4 * i + 3));
-        return (((sign) ? "" : "-") + "$" + num + '.' + cents);
-    }
-}
-
-
-function post_form(formobj) {
-    var datatogo = jQuery('#' + formobj.name).serializeObject();
-    if (formobj.name == 'I.BLAH.EDIT') {
-        var request = jQuery.ajax({
-            url: "/x/moneywatch-relay.py",
-           type: "POST",
-           data: datatogo,
-           dataType: "html"
-        });
-        request.done(function(indata) {
-            jQuery('#paneluniversal-inner').html(indata);
-            panelUniversal.show();
-        });
-        //request.fail(function( jqXHR, textStatus ) {
-        //    alert( "Request failed: " + textStatus );
-        //});
-    }
-}
-
-
 
 
 function addbank_typechange() {
@@ -670,6 +476,26 @@ function addbank_payeechange() {
     }
 }
 
+// ==== UTILITY
+
+function poisonURL() {
+    return new Date().getTime();
+}
+
+function formatCurrency(num) {
+    if (typeof num != 'undefined') {
+        num = num.toString().replace(/\$|\,/g, '');
+        if (isNaN(num)) num = "0";
+        sign = (num == (num = Math.abs(num)));
+        num = Math.floor(num * 100 + 0.50000000001);
+        cents = num % 100;
+        num = Math.floor(num / 100).toString();
+        if (cents < 10) cents = "0" + cents;
+        for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
+        num = num.substring(0, num.length - (4 * i + 3)) + ',' + num.substring(num.length - (4 * i + 3));
+        return (((sign) ? "" : "-") + "$" + num + '.' + cents);
+    }
+}
 
 function checkValueDecimals(in_obj, places) {
     // converts a number to a dollar (two decimals)  450.95 format
