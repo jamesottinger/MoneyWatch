640d32eefd7c83d4ab6de6a4a2f3c56133d36cf7
James Ottinger <ottinger.james@gmail.com>
2014-10-18 16:50:47 -0400

    add a proper main to relay file

--- cgi-bin/moneywatch-relay.py
+++ cgi-bin/moneywatch-relay.py
@@ -48,88 +48,92 @@ U.UPDATEQUOTES
 def p_content():
     print "Content-type: text/html\n\n"
 
-if moneywatchengine.g_formdata.getvalue('job') == 'I.SUMMARY.GET':
-    p_content()
-    print moneywatchengine.i_summary()
-elif moneywatchengine.g_formdata.getvalue('job') == 'I.ELECTION.GET':
-    p_content()
-    print moneywatchengine.i_electionget()
-elif moneywatchengine.g_formdata.getvalue('job') == 'I.BULKADD.EDIT':
-    p_content()
-    print moneywatchengine.i_bulkadd_edit()
-elif moneywatchengine.g_formdata.getvalue('job') == 'I.BULKADD.SAVE':
-    p_content()
-    moneywatchengine.i_bulkadd_save()
-    print "ok"
-elif moneywatchengine.g_formdata.getvalue('job') == 'I.ENTRY.ADD':
-    p_content()
-    print moneywatchengine.i_entry_prepareadd()
-elif moneywatchengine.g_formdata.getvalue('job') == 'I.ENTRY.EDIT':
-    p_content()
-    print moneywatchengine.i_entry_prepareedit()
-elif moneywatchengine.g_formdata.getvalue('job') == 'I.ENTRY.ADDSAVE' or moneywatchengine.g_formdata.getvalue('job') == 'I.ENTRY.EDITSAVE':
-    p_content()
-    moneywatchengine.i_prepare_addupdate()
-    print "ok"
-elif moneywatchengine.g_formdata.getvalue('job') == 'I.ENTRY.DELETE':
-    p_content()
-    moneywatchengine.i_entry_delete()
-    print "ok"
-elif moneywatchengine.g_formdata.getvalue('job') == 'I.GRAPH.GET':
-    p_content()
-    print moneywatchengine.i_graph()
-elif moneywatchengine.g_formdata.getvalue('job') == 'B.SUMMARY.GET':
-    p_content()
-    print moneywatchengine.b_summary()
-elif moneywatchengine.g_formdata.getvalue('job') == 'B.ACCOUNT.GET':
-    p_content()
-    print moneywatchengine.b_accountget()
-elif moneywatchengine.g_formdata.getvalue('job') == 'B.ENTRY.ADD':
-    p_content()
-    print moneywatchengine.b_entry_prepareadd()
-elif moneywatchengine.g_formdata.getvalue('job') == 'B.ENTRY.EDIT':
-    p_content()
-    print moneywatchengine.b_entry_prepareedit()
-elif moneywatchengine.g_formdata.getvalue('job') == 'B.ENTRY.ADDSAVE' or moneywatchengine.g_formdata.getvalue('job') == 'B.ENTRY.EDITSAVE':
-    p_content()
-    moneywatchengine.b_prepare_addupdate()
-    print "ok"
-elif moneywatchengine.g_formdata.getvalue('job') == 'B.ENTRY.DELETE':
-    p_content()
-    moneywatchengine.b_entry_delete()
-    print "ok"
-elif moneywatchengine.g_formdata.getvalue('job') == 'B.BULKINTEREST.EDIT':
-    p_content()
-    print moneywatchengine.b_bulkinterest_edit()
-elif moneywatchengine.g_formdata.getvalue('job') == 'B.BULKINTEREST.SAVE':
-    p_content()
-    moneywatchengine.b_bulkinterest_save()
-    print "ok"
-elif moneywatchengine.g_formdata.getvalue('job') == 'B.BULKBILLS.EDIT':
-    p_content()
-    print moneywatchengine.b_bulkbills_edit()
-elif moneywatchengine.g_formdata.getvalue('job') == 'B.BULKBILLS.SAVE':
-    p_content()
-    moneywatchengine.b_bulkbills_save()
-    print "ok"
-elif moneywatchengine.g_formdata.getvalue('job') == 'U.IMPORTFILE.EDIT':
-    p_content()
-    print moneywatchengine.u_importfile_edit()
-elif moneywatchengine.g_formdata.getvalue('job') == 'U.IMPORTFILE.SAVE':
-    p_content()
-    print moneywatchengine.u_importfile_save()
-elif moneywatchengine.g_formdata.getvalue('job') == 'U.UPDATEQUOTES':
-    p_content()
-    moneywatchengine.u_fetchquotes()
-    print "ok"
-elif moneywatchengine.g_formdata.getvalue('job') == 'U.UPDATEBANKTOTALS':
-    p_content()
-    moneywatchengine.u_banktotals()
-    print "ok"
-else:
-    p_content()
-    print ")("# invalid, print ass cheeks
+def main():
+    if moneywatchengine.g_formdata.getvalue('job') == 'I.SUMMARY.GET':
+        p_content()
+        print moneywatchengine.i_summary()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'I.ELECTION.GET':
+        p_content()
+        print moneywatchengine.i_electionget()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'I.BULKADD.EDIT':
+        p_content()
+        print moneywatchengine.i_bulkadd_edit()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'I.BULKADD.SAVE':
+        p_content()
+        moneywatchengine.i_bulkadd_save()
+        print "ok"
+    elif moneywatchengine.g_formdata.getvalue('job') == 'I.ENTRY.ADD':
+        p_content()
+        print moneywatchengine.i_entry_prepareadd()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'I.ENTRY.EDIT':
+        p_content()
+        print moneywatchengine.i_entry_prepareedit()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'I.ENTRY.ADDSAVE' or moneywatchengine.g_formdata.getvalue('job') == 'I.ENTRY.EDITSAVE':
+        p_content()
+        moneywatchengine.i_prepare_addupdate()
+        print "ok"
+    elif moneywatchengine.g_formdata.getvalue('job') == 'I.ENTRY.DELETE':
+        p_content()
+        moneywatchengine.i_entry_delete()
+        print "ok"
+    elif moneywatchengine.g_formdata.getvalue('job') == 'I.GRAPH.GET':
+        p_content()
+        print moneywatchengine.i_graph()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'B.SUMMARY.GET':
+        p_content()
+        print moneywatchengine.b_summary()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'B.ACCOUNT.GET':
+        p_content()
+        print moneywatchengine.b_accountget()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'B.ENTRY.ADD':
+        p_content()
+        print moneywatchengine.b_entry_prepareadd()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'B.ENTRY.EDIT':
+        p_content()
+        print moneywatchengine.b_entry_prepareedit()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'B.ENTRY.ADDSAVE' or moneywatchengine.g_formdata.getvalue('job') == 'B.ENTRY.EDITSAVE':
+        p_content()
+        moneywatchengine.b_prepare_addupdate()
+        print "ok"
+    elif moneywatchengine.g_formdata.getvalue('job') == 'B.ENTRY.DELETE':
+        p_content()
+        moneywatchengine.b_entry_delete()
+        print "ok"
+    elif moneywatchengine.g_formdata.getvalue('job') == 'B.BULKINTEREST.EDIT':
+        p_content()
+        print moneywatchengine.b_bulkinterest_edit()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'B.BULKINTEREST.SAVE':
+        p_content()
+        moneywatchengine.b_bulkinterest_save()
+        print "ok"
+    elif moneywatchengine.g_formdata.getvalue('job') == 'B.BULKBILLS.EDIT':
+        p_content()
+        print moneywatchengine.b_bulkbills_edit()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'B.BULKBILLS.SAVE':
+        p_content()
+        moneywatchengine.b_bulkbills_save()
+        print "ok"
+    elif moneywatchengine.g_formdata.getvalue('job') == 'U.IMPORTFILE.EDIT':
+        p_content()
+        print moneywatchengine.u_importfile_edit()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'U.IMPORTFILE.SAVE':
+        p_content()
+        print moneywatchengine.u_importfile_save()
+    elif moneywatchengine.g_formdata.getvalue('job') == 'U.UPDATEQUOTES':
+        p_content()
+        moneywatchengine.u_fetchquotes()
+        print "ok"
+    elif moneywatchengine.g_formdata.getvalue('job') == 'U.UPDATEBANKTOTALS':
+        p_content()
+        moneywatchengine.u_banktotals()
+        print "ok"
+    else:
+        p_content()
+        print ")("# invalid, print ass cheeks
 
     #moneywatchengine.b_accounttally(1)
     #print moneywatchengine.i_entry_prepareedit()
+
+if __name__ == '__main__':
+    main()
 
