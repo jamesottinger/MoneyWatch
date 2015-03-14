20a1ba3d812fadeefadb858dc7ef4ce4d864a40c
James Ottinger <ottinger.james@gmail.com>
2015-03-14 17:25:06 -0400

    use strict for formatCurrency

--- js/moneywatch_new.js
+++ js/moneywatch_new.js
@@ -498,11 +498,12 @@ MW.util = {
     // ------------------------------------------------------------------
     formatCurrency: function (num) {
         if (typeof num != 'undefined') {
+            var cents, sign;
             num = num.toString().replace(/\$|\,/g, '');
             if (isNaN(num)) num = "0";
-            var sign = (num == (num = Math.abs(num)));
-            var num = Math.floor(num * 100 + 0.50000000001);
-            var cents = num % 100;
+            sign = (num == (num = Math.abs(num)));
+            num = Math.floor(num * 100 + 0.50000000001);
+            cents = num % 100;
             num = Math.floor(num / 100).toString();
             if (cents < 10) cents = "0" + cents;
             for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
