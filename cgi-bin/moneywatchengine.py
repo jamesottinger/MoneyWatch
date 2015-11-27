6bf87c0f6e4f52e9545501288906ae5adf5d9507
James Ottinger <ottinger.james@gmail.com>
2015-11-27 14:11:14 -0500

    switch main engine to python 3, update all calls

--- cgi-bin/moneywatchengine.py
+++ cgi-bin/moneywatchengine.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/ usr/bin/env python3
 #===============================================================================
-# Copyright (c) 2014, James Ottinger. All rights reserved.
+# Copyright (c) 2015, James Ottinger. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
