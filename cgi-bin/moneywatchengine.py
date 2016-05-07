455ea4288176143e6be026c25f3579da466dd5a2
James Ottinger <ottinger.james@gmail.com>
2016-05-07 14:53:45 -0400

    it's a new year

--- cgi-bin/moneywatchengine.py
+++ cgi-bin/moneywatchengine.py
@@ -1,6 +1,6 @@
 #!/ usr/bin/env python3
 # ===============================================================================
-# Copyright (c) 2015, James Ottinger. All rights reserved.
+# Copyright (c) 2016, James Ottinger. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
