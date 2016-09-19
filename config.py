9ffd73686aa709d86ae6f0a9f661242f77722a1b
James Ottinger <ottinger.james@gmail.com>
2016-09-18 23:19:46 -0400

    add config

---
+++ config.py
@@ -0,0 +1,31 @@
+# -*- coding: utf8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+
+    @staticmethod
+    def init_app(moneywatch):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    #    'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+    #    'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    # WTF_CSRF_ENABLED = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'default': DevelopmentConfig
+}
