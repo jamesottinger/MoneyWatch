6d17ce2982e98953438d5e58c713a0528ed01325
James Ottinger <ottinger.james@gmail.com>
2016-09-18 23:25:37 -0400

    add moneywatchconfig file

---
+++ moneywatch/moneywatchconfig.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#===============================================================================
+# Copyright (c) 2016, James Ottinger. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# MoneyWatch - https://github.com/jamesottinger/moneywatch
+#=======================================================+=======================
+db_creds   = { 'host':'xxx.xxx.xxx.xxx', 'user':'youruser', 'passwd':'yourpassword', 'db':'thedbname' }
+dirlogs    = '/log/'
+direrrors  = '/log/errors/'
+weather = { 'latitude':'39.7344443', 'longitude':'-75.0072787', 'title':'Chipotle - Sicklerville, NJ' }
+
+uilinks = [
+            [
+                ('Google Drive', 'https://drive.google.com/#my-drive')
+            ], [
+                ('Capital One 360', 'https://home.capitalone360.com'),
+                ('Discover Bank', 'https://www.discover.com/online-banking/')
+            ], [
+                ('Fidelity', 'https://www.fidelity.com'),
+                ('Vanguard', 'https://www.vanguard.com/')
+            ]
+        ]
