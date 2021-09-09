27d741f0b2d18925b616c0a7a3f5e11f82faf694
James Ottinger <ottinger.james@gmail.com>
2021-09-08 22:12:39 -0400

    update requirements for Flask-Executor update init
    Adding futures to handle long running requests (fetching quotes)

--- moneywatch/__init__.py
+++ moneywatch/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_executor import Executor
 from config import config
 from . import moneywatchengine, moneywatchconfig, views
 
+executor = Executor()
 
 def create_app(config_name="default"):
     moneywatch = Flask(__name__)
@@ -10,5 +12,6 @@ def create_app(config_name="default"):
 
     from . views import relay as main_blueprint
     moneywatch.register_blueprint(main_blueprint)
+    executor.init_app(app)
 
     return moneywatch
